867ffe2277eecb754752fbec33868b01d4e762be
Samuel <samukfer@gmail.com>
2020-02-07 00:26:05 -0300

    data no cadastro e outros

--- src/cadForm.js
+++ src/cadForm.js
@@ -9,12 +9,19 @@ import {
   TextInput,
   TouchableOpacity,
   KeyboardAvoidingView,
+  DatePickerAndroid,
 } from 'react-native';
 import ImagePicker from 'react-native-image-picker';
 
 class CadForm extends Component {
   state = {
     photo: null,
+    nome: '',
+    sobrenome: '',
+    email: '',
+    senha: '',
+    senhaConf: '',
+    data: '',
   };
 
   async acesso() {
@@ -72,10 +79,20 @@ class CadForm extends Component {
           placeholderTextColor="rgba(255,255,255,0.7)"
           placeholder="nome"
           returnKeyType="next"
+          onSubmitEditing={() => this.sobrenomeInput.focus()}
+          autoCapitalize="none"
+          onChangeText={text => this.setState({nome: text})}
+          style={[styles.input, styles.botaoFoto]}
+        />
+        <TextInput
+          placeholderTextColor="rgba(255,255,255,0.7)"
+          placeholder="sobrenome"
+          returnKeyType="next"
           onSubmitEditing={() => this.emailInput.focus()}
           autoCapitalize="none"
-          onChangeText={text => this.setState({email: text})}
-          style={[styles.input, styles.botaoFoto]}
+          ref={input => (this.sobrenomeInput = input)}
+          onChangeText={text => this.setState({sobrenome: text})}
+          style={styles.input}
         />
         <TextInput
           placeholderTextColor="rgba(255,255,255,0.7)"
@@ -92,11 +109,44 @@ class CadForm extends Component {
           placeholderTextColor="rgba(255,255,255,0.7)"
           placeholder="senha"
           secureTextEntry
-          returnKeyType="go"
+          returnKeyType="next"
           ref={input => (this.passwordInput = input)}
+          onSubmitEditing={() => this.passwordConfInput.focus()}
           onChangeText={text => this.setState({senha: text})}
           style={styles.input}
         />
+        <TextInput
+          placeholderTextColor="rgba(255,255,255,0.7)"
+          placeholder="confirmar senha"
+          secureTextEntry
+          returnKeyType="next"
+          ref={input => (this.passwordConfInput = input)}
+          onSubmitEditing={() => this.dataInput.focus()}
+          onChangeText={text => this.setState({senhaConf: text})}
+          style={styles.input}
+        />
+        <TouchableOpacity
+          onPress={async() =>
+            {
+              const {year, month, day} = await DatePickerAndroid.open({
+                date: this.state.data,
+                mode: 'spinner',
+              });
+              var data = new Date(String(year) + "-" + String(month) + "-" + String(day));
+              this.setState({
+                data: data,
+              });
+            }
+          }>
+          <TextInput
+            placeholderTextColor="rgba(255,255,255,0.7)"
+            returnKeyType="go"
+            ref={input => (this.dataInput = input)}
+            onChangeText={text => this.setState({data: text})}
+            value={new Date.getDate()}
+            style={styles.input}
+          />
+        </TouchableOpacity>
         <TouchableOpacity
           style={styles.botao}
           onPress={() =>
